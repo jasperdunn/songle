ad3ee4b1276b98b83e7662f142ac2dc84008b1cd
Jasper Dunn <jasperdunn93@gmail.com>
2022-04-13 18:51:50 +1000

    refactor config files and replace jest with vitest

--- src/components/Game/utils.test.ts
+++ src/components/Game/utils.test.ts
@@ -1,21 +1,13 @@
 import { validate } from 'components/Game/utils'
-import each from 'jest-each'
-import { attempts, ValidateParams } from './utils.test.data'
+import { test, describe, expect } from 'vitest'
+import { attempts, ValidateParams } from './utils.test-data'
 
-each`
-  attemptedSequence                | challengeSequence                | expected
-  ${attempts[0].attemptedSequence} | ${attempts[0].challengeSequence} | ${attempts[0].expected}
-  ${attempts[1].attemptedSequence} | ${attempts[1].challengeSequence} | ${attempts[1].expected}
-  ${attempts[2].attemptedSequence} | ${attempts[2].challengeSequence} | ${attempts[2].expected}
-`.describe(
-  'validate',
-  ({ attemptedSequence, expected, challengeSequence }: ValidateParams) => {
-    it(`"${JSON.stringify(attemptedSequence)}" from "${JSON.stringify(
-      challengeSequence
-    )}" returns "${JSON.stringify(expected)}" `, () => {
+describe('validate', () => {
+  test.each(attempts)(
+    '%j from %j returns %j',
+    ({ attemptedSequence, expected, challengeSequence }: ValidateParams) => {
       const actual = validate(attemptedSequence, challengeSequence)
-
       expect(actual).toStrictEqual(expected)
-    })
-  }
-)
+    }
+  )
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
@@ -9,4 +11,9 @@ export default defineConfig({
     react(),
     tsconfigPaths({ projects: [resolve(__dirname, 'tsconfig.json')] }),
   ],
+  // https://vitest.dev/config/#configuration
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
