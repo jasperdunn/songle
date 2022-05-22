20dab2309fb95e474029194e608ae6793c250bee
Jasper Dunn <jasperdunn93@gmail.com>
2022-05-23 09:37:27 +1000

    fix Modal statistics

--- src/components/Game/GameOverModal/Statistics/index.tsx
+++ src/components/Game/GameOverModal/Statistics/index.tsx
@@ -2,12 +2,12 @@ import { GameContext } from 'components/Game/context'
 import { useContext } from 'react'
 
 export function Statistics(): JSX.Element {
-  const { attempts, currentAttemptIndex } = useContext(GameContext)
+  const { attempts, listenableAttemptIndex } = useContext(GameContext)
 
   return (
     <>
       <h2>Statistics</h2>
-      Completed attempt: {currentAttemptIndex + 1} / {attempts.length}
+      Completed attempt: {listenableAttemptIndex} / {attempts.length}
     </>
   )
 }
